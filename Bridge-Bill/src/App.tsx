0aeaada8ecf123c131f36d4e8d189db5b8509e3a
UTShilpi <avanish.shilpi@utexas.edu>
2024-03-30 01:13:38 -0500

    made great progress to the first page of code and tyring to import a file

--- Bridge-Bill/src/App.tsx
+++ Bridge-Bill/src/App.tsx
@@ -1,28 +1,51 @@
-import "./styles.css";
-import ImportPdf from "./ImportPDF";
+import "./styles.css"
+import React, { useState } from 'react';
+import ImportPdf from './ImportPDF.tsx'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a className="top-left-text">English</a>
-        <p>
-          The billing assistant<br></br> your health deserves.
-        </p>
-        <a className="subheading">
-          Your Guide to Clear and Accurate Medical Billing
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Started
-        </a>
-      </header>
-    </div>
+
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+  const openPopup = () => setIsPopupOpen(true);
+  const closePopup = () => setIsPopupOpen(false);
+
+
+  return(
+    <body>
+        <div className="container">
+            <header>
+                <div className="language-selector">
+                    <span>English</span>
+                </div>
+            </header>
+            <main>
+                <h1>The billing assistant your <br></br> health deserves.</h1>
+                <p>Your Guide to Clear and Accurate Medical Billing</p>
+                <div>
+                  <button className="get-started-btn"onClick={openPopup}>Get Started</button>
+                  <ImportPdf isOpen={isPopupOpen} onClose={closePopup} />
+                </div>
+            </main>
+        </div>
+    </body>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="Title">
+  //       <a className="language-chooser">English</a>
+  //       <p className="Title">
+  //         The billing assistant<br></br> your health deserves.
+  //       </p>
+  //       <a className="subheading">
+  //         Your Guide to Clear and Accurate Medical Billing
+  //       </a>
+  //       <div>
+  //         <button onClick={openPopup}>Insert PDF</button>
+  //         <ImportPdf isOpen={isPopupOpen} onClose={closePopup} />
+  //       </div>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
