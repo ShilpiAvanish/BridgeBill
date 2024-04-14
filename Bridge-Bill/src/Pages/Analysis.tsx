92e9363dae41fbcdd59aa29b0fbc734017456015
ShilpiAvanish <ShilpiAvanish25@gmail.com>
2024-04-13 23:13:24 -0500

    finished backend

--- Bridge-Bill/src/Pages/Analysis.tsx
+++ Bridge-Bill/src/Pages/Analysis.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-function Analysis() {
-    return(
-        <div>
-            PDF Analyss
-        </div>
-    )
-}
+const Analysis = () => {
+  const location = useLocation();
+  
+  // Make sure 'data' is the correct key where the server processing result is being stored.
+  // If 'data' does not exist or if the structure is different, it won't work as expected.
+  const analysisData = location.state?.data;
 
-export default Analysis
+  console.log(analysisData);
+  console.log(location);
+  return (
+    <div>
+      <h1>Analysis Results</h1>
+      {/* Ensure that analysisData contains the data you need to display */}
+      {/* If analysisData is undefined or null, JSON.stringify will output an empty string */}
+      <pre>{JSON.stringify(analysisData, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Analysis;
